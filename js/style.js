8a46f313c662591752a012ec44ab46ddfb83d830
Jane Stanton <stanjane@gmail.com>
2015-01-30 20:37:25 -0600

    ch

--- js/style.js
+++ js/style.js
@@ -1,17 +1,20 @@
-$(".service").click(function() {
+
+
+
+(".service").click(function() {
     $('html, body').animate({
         scrollTop: $(".target_service").offset().top
     }, 500);
 });
 
-$(".about").click(function() {
+(".about").click(function() {
     $('html, body').animate({
         scrollTop: $(".target_about").offset().top
     }, 500);
 });
 
 
-$(".contact").click(function() {
+(".contact").click(function() {
     $('html, body').animate({
         scrollTop: $(".target_contact").offset().top
     }, 500);
