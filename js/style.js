ee235a57150ff8a4fbef79bd7b60ec83c0c56f4d
Jane Stanton <stanjane@gmail.com>
2015-01-30 20:24:29 -0600

    ch

---
+++ js/style.js
@@ -0,0 +1,18 @@
+$(".service").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".target_service").offset().top
+    }, 500);
+});
+
+$(".about").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".target_about").offset().top
+    }, 500);
+});
+
+
+$(".contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".target_contact").offset().top
+    }, 500);
+});
